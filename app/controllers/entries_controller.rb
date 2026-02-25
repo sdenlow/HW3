aa08b4e95eac4a3b7197c87cc9ad1f501c6ba1d7
sdenlow <sydney.denlow@kellogg.northwestern.edu>
2026-02-25 02:23:39 +0000

    so close to being done

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,37 +1,24 @@
 class EntriesController < ApplicationController
-def index
+  def index
+  end
 
   def show
     @entry = Entry.find_by({"id" => params["id"]})
-    @places = Place.find_by({"id" => @entry["place_id"]})
-    # render places/show view with details about Entry
+    @place = Place.find_by({"id" => @entry["place_id"]})
   end
 
   def new
     @place = Place.find_by({ "id" => params["place_id"] })
-    # render entry/new view with new entry form
   end
 
   def create
-    # start with a new Entry
     @entry = Entry.new
-
-    # assign user-entered form data to Entry's columns
     @entry["title"] = params["title"]
     @entry["description"] = params["description"]
     @entry["occurred_on"] = params["occurred_on"]
-
-    # assign relationship between Entry and Place
     @entry["place_id"] = params["place_id"]
-
-    # save Entry row
     @entry.save
-
-    # redirect user
     redirect_to "/places/#{@entry["place_id"]}"
   end
-
-end
-
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # get("/", { :controller => "articles", :action => "index" })
+  
 get("/places", { :controller => "places", :action => "index" })
 get("/places/new", { :controller => "places", :action => "new" })
 post("/places", { :controller => "places", :action => "create" })
