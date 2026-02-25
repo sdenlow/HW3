f54f5cbb470c552c959be502158791a681951724
sdenlow <sydney.denlow@kellogg.northwestern.edu>
2026-02-25 03:08:42 +0000

    late night update

--- scripts/create_data.rb
+++ scripts/create_data.rb
@@ -25,12 +25,14 @@ tacos["title"] = "Ate tacos"
 tacos["description"] = "they were spicy"
 tacos["occurred_on"] = "2022-01-01"
 tacos["place_id"] = mexico.id
+tacos.save
 
 tacos = Entry.new
 tacos["title"] = "Lisa and Jim's Wedding"
 tacos["description"] = "dance dance dance"
 tacos["occurred_on"] = "2022-01-01"
 tacos["place_id"] = mexico.id
+tacos.save
 
 tacos = Entry.new
 tacos["title"] = "Ate tacos again"
@@ -38,3 +40,24 @@ tacos["description"] = "Now they were sweet!"
 tacos["occurred_on"] = "2022-01-01"
 tacos["place_id"] = mexico.id
 tacos.save
+
+historic = Entry.new
+historic["title"] = "carriage ride"
+historic["description"] = "it was a lovely ride through the city"
+historic["occurred_on"] = "2022-04-01"
+historic["place_id"] = charleston.id
+historic.save
+
+iconic = Entry.new
+iconic["title"] = "Great Wall of China"
+iconic["description"] = "we hiked and hiked and hiked"
+iconic["occurred_on"] = "2022-04-20"
+iconic["place_id"] = beijing.id
+iconic.save
+
+charm = Entry.new
+charm["title"] = "Van Gogh Museum"
+charm["description"] = "saw some famous paintings"
+charm["occurred_on"] = "2022-04-22"
+charm["place_id"] = amsterdam.id
+charm.save
